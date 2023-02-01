e244b8c2ac7a4478099bb79338c12f8609add332
Alishkoo <baisholanov.alibek@gmail.com>
2023-02-01 22:40:43 +0600

    adding

---
+++ lab2/ifelse/2.py
@@ -0,0 +1,16 @@
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+
+
+if b > a:
+  pass
+
+if not a > b:
+  print("a is NOT greater than b")
