d224f675bacac7eb132fc04933c4ed61a5ea8a7e
Alishkoo <baisholanov.alibek@gmail.com>
2023-02-10 22:48:44 +0600

    lab3

--- lab3/class/6.py
+++ lab3/class/6.py
@@ -1,7 +1,13 @@
-nums = input().split(' ')
+def filter_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
 
-def filter():
-    pass
-   
+num = list(map(int, input().split()))
 
-filter()
+prime_numbers = list(filter(lambda x: filter_prime(x), num))
+
+print("Prime numbers in the list:", prime_numbers)
