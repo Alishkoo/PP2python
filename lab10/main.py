616bf77ce88a10cde550ebd655697a07a47a12e0
Alishkoo <baisholanov.alibek@gmail.com>
2023-04-23 16:41:57 +0600

    lab10

---
+++ lab10/main.py
@@ -0,0 +1,31 @@
+import csv
+import psycopg2
+from config import host, user, password, db_name
+
+try:
+    #connect to exist database
+    connection = psycopg2.connect(
+        host = host, 
+        user = user,
+        password = password,
+        database = db_name
+    )
+    connection.autocommit = True
+    
+    #cursor for performing database operations
+    # cursor = connection.cursor()
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT version();"
+        )
+        
+        print(f"Server version: {cursor.fetchone()}")
+
+except Exception as _ex:
+    print("[INFO] Error while working with PostgreSQL", _ex)
+    
+finally:
+    if connection:
+        # cursor.close()
+        connection.close()
+        print("[INFO] PostgreSQL connection closed")
